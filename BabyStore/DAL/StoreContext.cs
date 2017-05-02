66ab197ee5b8551b0c547042def8514161814a1e
Brendon Hollingsworth <brendon.r.hollingsworth@outlook.com>
2017-05-02 09:50:01 -0400

    inital commit

---
+++ BabyStore/DAL/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using BabyStore.Models;
+
+namespace BabyStore.DAL
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ BabyStore/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BabyStore.Models
+{
+    public class Category
+    {
+
+        public int ID { get; set; }
+        [Required(ErrorMessage ="The Name can not be blank")]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="Name must be longer then 3 Char")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage ="Must be letters only")]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
