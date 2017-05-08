3e01734f7036d2943bae9144174d070ed15eda83
Brendon Hollingsworth <brendon.r.hollingsworth@outlook.com>
2017-05-08 12:01:56 -0400

    Updated product Image file locations

---
+++ BabyStore/Controllers/ProductImagesController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BabyStore.DAL;
+using BabyStore.Models;
+using System.Web.Helpers;
+
+namespace BabyStore.Controllers
+{
+    public class ProductImagesController : Controller
+    {
+        private StoreContext db = new StoreContext();
+
+        // GET: ProductImages
+        public ActionResult Index()
+        {
+            return View(db.ProductImages.ToList());
+        }
+
+        // GET: ProductImages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductImage productImage = db.ProductImages.Find(id);
+            if (productImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productImage);
+        }
+
+        // GET: ProductImages/Create
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        // POST: ProductImages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                if (ValidateFile(file))
+                {
+                    try
+                    {
+                        SaveFileToDisk(file);
+                    }
+                    catch(Exception)
+                    {
+                        ModelState.AddModelError("FileName", "Sorry an error occurred saving file");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("FileName", "File must be gif, png, jpeg, jpg and smaller thne 2mb in size");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("FileName", "Please choose a file");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.ProductImages.Add(new ProductImage { FileName = file.FileName });
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // GET: ProductImages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductImage productImage = db.ProductImages.Find(id);
+            if (productImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productImage);
+        }
+
+        // POST: ProductImages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FileName")] ProductImage productImage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productImage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productImage);
+        }
+
+        // GET: ProductImages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductImage productImage = db.ProductImages.Find(id);
+            if (productImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productImage);
+        }
+
+        // POST: ProductImages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductImage productImage = db.ProductImages.Find(id);
+            db.ProductImages.Remove(productImage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private bool ValidateFile(HttpPostedFileBase file)
+        {
+            string fileExtension = System.IO.Path.GetExtension(file.FileName).ToLower();
+            string[] allowedFileTypes = { ".gif", ".png", ".jpeg", ".jpg" };
+            if((file.ContentLength > 0 && file.ContentLength < 2097152) && allowedFileTypes.Contains(fileExtension))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void SaveFileToDisk(HttpPostedFileBase file)
+        {
+            WebImage img = new WebImage(file.InputStream);
+            if (img.Width > 190)
+            {
+                img.Resize(190, img.Height);
+            }
+            img.Save(Constants.ProductImagePath + file.FileName);
+            if (img.Width > 100)
+            {
+                img.Resize(190, img.Height);
+            }
+            img.Save(Constants.ProductThumbnailPath + file.FileName);
+        }
+    }
+}
--- BabyStore/Controllers/ProductsController.cs
+++ BabyStore/Controllers/ProductsController.cs
@@ -59,9 +59,9 @@ namespace BabyStore.DAL
                     products = products.OrderBy(p => p.Name);
                     break;
             }
-            const int PageItems = 3;
+            
             int currentPage = (page ?? 1);
-            viewModel.Products = products.ToPagedList(currentPage, PageItems);
+            viewModel.Products = products.ToPagedList(currentPage, Constants.PageItems);
             viewModel.SortBy = sortBy;
             viewModel.Sorts = new Dictionary<string, string>
             {
